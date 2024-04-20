2e5989353d794803ee974664a4f3dfd00463feca
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-04-20 19:40:39 -0400

    -Added destinations feature

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']},
+  arrival: {
+    type: Date,
+    default: function() {
+      const today = new Date();
+      return new Date().setFullYear(today.getFullYear() + 1);
+    }
+  }
+}, {
+  timestamps: true
+});
+
 const flightsSchema = new mongoose.Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United'] },
   airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']},
@@ -13,6 +26,7 @@ const flightsSchema = new mongoose.Schema({
       return new Date().setFullYear(today.getFullYear() + 1);
     }
   },
+  destinations: [destinationSchema]
 },);
 
 // Compile the schema into a model and export it
