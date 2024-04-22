792924c91428fc8fa57d52c9f1eac1c7e7d7066b
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-04-22 08:51:35 -0400

    -Added last bit of ticket feature, working on ticket bug

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -2,22 +2,9 @@ const Flight = require('../models/flight');
 const Ticket = require('../models/ticket');
 
 module.exports = {
-  index,
-  show,
   new: newTicket,
   create
 };
-
-async function index(req, res) {
-  const flights = await Flight.find({});
-  res.render('flights/index', { title: 'All flights', flights });
-}
-
-async function show(req, res) {
-  const flight = await Flight.findById(req.params.id);
-  const tickets = await Ticket.find({flight: flight._id});
-  res.render('flights/show', { title: 'Flight Detail', flight });
-}
 
 function newTicket(req, res) {
   res.render('flights/new', { title: 'Add Flight', errorMsg: '' });
